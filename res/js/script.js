ab84c467715664da0d7aecf0cc1c9526c9a586f2
APAlango <56919413+APAlango@users.noreply.github.com>
2020-10-22 13:48:17 +0300

    Successfully added dropdown menu with fetched userdata

--- res/js/script.js
+++ res/js/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
   // jQuery methods go here...
+  $(".dropdownMe").hide();
+  $.get("https://private-anon-735fc5c29c-wad20postit.apiary-mock.com/users/1", function(data, status){
+    console.log("Data: " + toString(data) + "\nStatus: " + status);
+    $("#avatar").attr("src", data.avatar);
+    console.log(data.avatar);
+    $("#_userName").text(data.firstname + " " + data.lastname);
+    $("#_userMail").text(data.email);
+  });
 
+
+  $("#avatar").click(function() {
+    $(".dropdownMe").toggle();
+  })
 });
 console.log("Scripts attached!");
