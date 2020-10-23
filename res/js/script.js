4453c2cd9a9f46d762e4b3592bf734843fde013b
stargateprovider <kana46@windowslive.com>
2020-10-23 10:55:38 +0300

    Completed task #2

--- res/js/script.js
+++ res/js/script.js
@@ -11,5 +11,54 @@ $(document).ready(function() {
   $("#avatar").click(function() {
     $(".dropdownMe").toggle();
   })
+
+  // Load posts
+  $.get("https://private-anon-32697201dd-wad20postit.apiary-mock.com/posts", (data, status) => {
+    let mediacontent, textcontent;
+
+    for (var i = 0; i < data.length; i++) {
+      let post = data[i];
+      textcontent = post.text !== null ? post.text : "";
+      
+      if (post.media === null) {
+        mediacontent = "";
+      } else if (post.media.type === "image") {
+        mediacontent = `<div class="post-image"><img src="${post.media.url}" alt=""></div>`;
+      } else if (post.media.type === "video") {
+        let format = post.media.url.slice(post.media.url.lastIndexOf(".") + 1);
+        mediacontent =
+          `<video controls>
+            <source src=" ${post.media.url}" type="video/${format}">
+          Your browser does not support the video tag.
+          </video>`;
+      }
+    
+      $(".main-container").append(
+        `<div class="post">
+          <div class="post-author">
+            <span class="post-author-info">
+              <img src="${post.author.avatar}" alt="Post author">
+              <small>${post.author.firstname} ${post.author.lastname}</small>
+            </span>
+            <small>${post.createTime}</small>
+          </div>
+          ${mediacontent}
+          <div class="post-title">
+            <h3>${textcontent}</h3>
+          </div>
+          <div class="post-actions">
+            <button type="button" name="like" class="like-button">${post.likes}</button>
+          </div>
+        </div>`
+      );
+    }
+
+    // Interactive like button for posts
+    $(".like-button").click(e => {
+      if (!e.target.classList.contains("liked")) {
+        e.target.className += " liked";
+      }
+    });
+  });
+
 });
-console.log("Scripts attached!");
